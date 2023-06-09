039be8c8e83d369c4f22bf14d3fa6903b16011e1
Qube404 <qube404@protonmail.com>
2023-06-10 05:09:17 +1000

    Added rotation to menu idle jump

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,7 @@ fn main() {
             (
                 bird::apply_bird_velocity,
                 bird::apply_bird_gravity,
+                bird::rotate_bird,
 
                 pipes::apply_pipes_velocity,
             )
@@ -58,8 +59,6 @@ fn main() {
             (
                 bird::move_bird,
                 pipes::move_pipes,
-
-                bird::rotate_bird,
 
                 bird::bird_pipe_collisions,
                 bird::bird_point_collisions,
